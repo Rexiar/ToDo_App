540641f596cae9d6e4abebd0279d90ab18d6e931
Rexiar <raditya.cbp@gmail.com>
2022-12-20 11:06:43 +0700

    hi

---
+++ ToDo_App/ToDo_App/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ToDo_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.activityListBox = new System.Windows.Forms.ListBox();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.testCheckBox = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(209, 307);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 0;
+            this.refreshButton.Text = "hey :)";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // activityListBox
+            // 
+            this.activityListBox.FormattingEnabled = true;
+            this.activityListBox.Location = new System.Drawing.Point(70, 112);
+            this.activityListBox.Name = "activityListBox";
+            this.activityListBox.Size = new System.Drawing.Size(250, 95);
+            this.activityListBox.TabIndex = 1;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(610, 112);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(100, 20);
+            this.titleTextBox.TabIndex = 2;
+            this.titleTextBox.Text = "Nama Aktivitas";
+            this.titleTextBox.TextChanged += new System.EventHandler(this.titleTextBox_TextChanged);
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(635, 332);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 3;
+            this.enterButton.Text = "button1";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(439, 306);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "deleteButton";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // testCheckBox
+            // 
+            this.testCheckBox.FormattingEnabled = true;
+            this.testCheckBox.Location = new System.Drawing.Point(364, 126);
+            this.testCheckBox.Name = "testCheckBox";
+            this.testCheckBox.Size = new System.Drawing.Size(120, 94);
+            this.testCheckBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.testCheckBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.activityListBox);
+            this.Controls.Add(this.refreshButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.ListBox activityListBox;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.CheckedListBox testCheckBox;
+    }
+}
+
---
+++ ToDo_App/ToDo_App/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace ToDo_App
+{
+    public partial class Form1 : Form
+    {
+        public static string getConnectionString() 
+        {
+            string basePath = Application.StartupPath;
+            int index = basePath.IndexOf("ToDo_App");
+            string path = basePath.Substring(0, index + @"ToDo_App\ToDo_App".Length) + @"\Database.mdf";
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="+path+ ";Integrated Security=True;Connect Timeout=30";
+            return connectionString;
+            //static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\USER\Code\C#\ToDo\ToDo_App\ToDo_App\Database.mdf;Integrated Security=True;Connect Timeout=30";
+
+        }
+        static string connectionString = getConnectionString();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void refreshTable()
+        {
+            SqlConnection dbConnection = new SqlConnection(connectionString);
+            dbConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from ActivityTracker", dbConnection);
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+            testCheckBox.Items.Clear();
+            foreach (DataTable dt in dataSet.Tables)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    testCheckBox.Items.Add(dr["Aktivitas"].ToString());
+                }
+            }
+            dbConnection.Close();
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            refreshTable();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection dbConnection = new SqlConnection(connectionString);
+            dbConnection.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO ActivityTracker (Aktivitas, Selesai) VALUES ('" + titleTextBox.Text + "','" + 1 + "')", dbConnection);
+            cmd.ExecuteNonQuery();
+            dbConnection.Close();
+            refreshTable();
+        }
+
+        private void titleTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection dbConnection = new SqlConnection(connectionString);
+            dbConnection.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM ActivityTracker WHERE Id='"+ testCheckBox.SelectedIndex+ "'", dbConnection);
+            Console.Write(testCheckBox.SelectedIndex);
+            cmd.ExecuteNonQuery();
+            dbConnection.Close();
+            refreshTable();
+        }
+    }
+}
